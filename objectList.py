8e44c41be6bf5884346e32055eb93d815ed0da60
Aniket Sonawane <aniket.sonawane1734@gmail.com>
2024-07-11 19:11:57 +0530

    added comments

--- objectList.py
+++ objectList.py
@@ -1,25 +1,24 @@
 import requests
 
-# Define the base URL for the Stellarium API
 base_url = 'http://localhost:8090/api/objects/'
 
-# Make the request to Stellarium API
 categories = ["NebulaMgr","NebulaMgr:5","StarMgr","SolarSystem"]
 
-
+# Returns type,rise and set time and magnitude of obj given
 def getObjDetails(objName):
-    print(objName)
     details = requests.get(f"{base_url}info?name={objName}&format=json").json()
     return {"type":details["type"],"rise":details["rise"],"set":details["set"],"mag":details["vmag"]}
 
+#contains keys as objname and values as its details dictionary
 objects_dict = {}
 
+
 for type in categories:
-    objects =eval( requests.get(f"{base_url}listobjectsbytype?type={type}").text)
+    objects =eval( requests.get(f"{base_url}listobjectsbytype?type={type}").text)#get list of objects of "type"
     for obj in objects:
         try:
             objects_dict[obj] = getObjDetails(obj)
-        except requests.exceptions.JSONDecodeError:
+        except requests.exceptions.JSONDecodeError:# when obj not found
             continue
 
 print(objects)
